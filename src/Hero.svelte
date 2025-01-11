d3db01fb8bf4cabdfc9209f7f42e075936fa48af
Hari Narayan <harinarayanmr@aol.com>
2025-01-11 07:25:39 +0530

    initialised capacitor

--- src/Hero.svelte
+++ src/Hero.svelte
@@ -3,6 +3,10 @@
   import Nav from "./Nav.svelte"
   import * as Card from "$lib/components/ui/card/index.js";
   import { Button } from "$lib/components/ui/button/index.js";
+
+  fetch('http://175.20.1.154:5555/send-data')
+  .then(r => r.text())
+  .then(text => console.log(text));
 </script>
 
 <main>
